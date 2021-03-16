5c18e2ef7f50827dfd9724624089ec03b73339c8
Rakesh Patole <patolerakesh@gmail.com>
2021-03-16 19:50:55 +0530

    added methods to build request and call api

--- Sources/HTTPClient/HTTPClient.swift
+++ Sources/HTTPClient/HTTPClient.swift
@@ -1,4 +1,5 @@
 import Foundation
+import Combine
 
 public enum HTTPError: Error {
     case noResponse
@@ -20,6 +21,56 @@ public enum HTTPContentType : String {
 }
 
 public protocol HTTPClient {
-    var baseURL : URL { get }
-    var host : URL { get }
+    var baseURL : URL! { get }
+    var host : URL! { get }
+    var additionalHeaders : [String:String]? { get set }
+}
+
+open class BaseHTTPClient : HTTPClient {
+    public var baseURL: URL!    
+    public var host: URL!
+    public var additionalHeaders: [String : String]?
+    
+    public init(baseURL:URL) {
+        self.baseURL = baseURL
+        self.host = baseURL
+    }
+    
+    open func headers<T:EndPointDescriptor>(endPoint:T) -> [String:String] {
+        return ["Origin":self.host.absoluteString,
+                "Content-Type":endPoint.contentType]
+            .merging(self.additionalHeaders ?? [:], uniquingKeysWith:{ $1 })
+            .merging(endPoint.headers, uniquingKeysWith:{ $1 })
+    }
+    
+    open func request<T:EndPointDescriptor>(endPoint:T) -> AnyPublisher<URLRequest,HTTPError> {
+        return Future <URLRequest,HTTPError> { [weak self]  promise in
+            do {
+                let request = try URLRequest(baseURL: self?.baseURL ?? URL(string: "http://")!)
+                    .method(endPoint.method)
+                    .path(endPoint.path)
+                    .params(endPoint.params)
+                    .headers(self?.headers(endPoint: endPoint) ?? [:])
+                    .body(endPoint.body())
+                             
+                promise(.success(request))
+            }
+            catch {
+                promise(.failure(HTTPError.networkError(error:error)))
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+    
+    open func call<T:EndPointDescriptor>(endPoint:T) -> AnyPublisher<T.Output,HTTPError>{
+        request(endPoint: endPoint)
+            .flatMap {
+                URLSession.shared
+                    .dataTaskPublisher(for: $0)
+                    .tryMap { try endPoint.deserializeResponse($0, response: $1)}
+                    .mapError { HTTPError.networkError(error: $0) }
+                    .receive(on: DispatchQueue.main)
+            }
+            .eraseToAnyPublisher()
+    }
 }
