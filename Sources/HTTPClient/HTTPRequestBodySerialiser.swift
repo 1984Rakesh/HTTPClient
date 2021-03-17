e06c44ae5c2cd5bf6400b43ca587b256b0271caf
Rakesh Patole <patolerakesh@gmail.com>
2021-03-17 11:13:12 +0530

    request body serialiser now accepts callback method to help create data required for serialisation

--- Sources/HTTPClient/HTTPRequestBodySerialiser.swift
+++ Sources/HTTPClient/HTTPRequestBodySerialiser.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Rakesh Patole on 13/03/21.
 //
@@ -9,21 +9,39 @@ import Foundation
 
 public protocol HTTPRequestBodySerialiser {
     associatedtype Input : Encodable
-    func serialise(_ params:Input) throws -> Data?
+    typealias SerialiserFunction = () throws -> Input
+    var serialiser : SerialiserFunction { get }
+    var contentType : String { get }
+    init(serialiser:@escaping SerialiserFunction)
+    func serialise() throws -> Data?
 }
 
 public struct JSONRequestBodySerialiser<T:Encodable> : HTTPRequestBodySerialiser {
     public typealias Input = T
-    public init() {}
-    public func serialise(_ params: T) throws -> Data? {
-        return try JSONEncoder().encode(params)
+    public var contentType: String {  HTTPContentType.json.rawValue }
+    public var serialiser: SerialiserFunction
+    
+    public init(serialiser:@escaping SerialiserFunction) {
+        self.serialiser = serialiser
+    }
+
+    public func serialise() throws -> Data? {
+        let param : T = try serialiser()
+        return try JSONEncoder().encode(param)
     }
 }
 
 public struct URLEncodedRequestSerialiser : HTTPRequestBodySerialiser {
     public typealias Input = [String:String]
-    public init() {}
-    public func serialise(_ params: [String : String]) throws -> Data? {
+    public var contentType: String {  HTTPContentType.urlEncoded.rawValue }
+    public var serialiser: SerialiserFunction
+    
+    public init(serialiser:@escaping SerialiserFunction) {
+        self.serialiser = serialiser
+    }
+    
+    public func serialise() throws -> Data? {
+        let params = try serialiser()
         let items = params.map { URLQueryItem(name: $0, value: $1) }
         var components = URLComponents()
         components.queryItems = items
