c0fc3fdd0afdb5da69f79343db485dc9447120f4
Rakesh Patole <patolerakesh@gmail.com>
2021-04-02 08:42:41 +0530

    added generic error

--- Sources/HTTPClient/HTTPInterface.swift
+++ Sources/HTTPClient/HTTPInterface.swift
@@ -6,6 +6,7 @@ public enum HTTPError: Error {
     case responseDeserialiseError(error: Error)
     case requestSerialiseError(String)
     case networkError(error: Error)
+    case genericError(String)
 }
 
 public enum HTTPMethod : String {
@@ -25,23 +26,35 @@ public protocol HTTPInterface {
     var baseURL : URL! { get }
     var host : URL! { get }
     var additionalHeaders : [String:String]? { get set }
+    var additionalURLParams: [String: String]? { get set }
+}
+
+extension HTTPInterface {
+    
 }
 
 open class BaseHTTPClient : HTTPInterface {
-    public var baseURL: URL!    
+    public var baseURL: URL!
     public var host: URL!
-    public var additionalHeaders: [String : String]?
+    open var additionalHeaders: [String : String]?
+    open var additionalURLParams: [String: String]?
     
     public init(baseURL:URL) {
         self.baseURL = baseURL
         self.host = baseURL
     }
     
-    open func headers<T:EndPointDescriptor>(endPoint:T) -> [String:String] {
+    open func headers<T:EndPointDescriptor>(_ endPoint:T) -> [String:String] {
         return ["Origin":self.host.absoluteString,
                 "Content-Type":endPoint.requestBodySerialiser?.contentType ?? HTTPContentType.none.rawValue]
-            .merging(self.additionalHeaders ?? [:], uniquingKeysWith:{ $1 })
-            .merging(endPoint.headers, uniquingKeysWith:{ $1 })
+            .merging(self.additionalHeaders ?? [:], uniquingKeysWith: { $1 })
+            .merging(endPoint.headers, uniquingKeysWith: { $1 })
+    }
+    
+    open func urlParams<T:EndPointDescriptor>(_ endPoint:T) -> [String:String]? {
+        return [String:String]()
+            .merging( self.additionalURLParams ?? [:], uniquingKeysWith: { $1 })
+            .merging( endPoint.params , uniquingKeysWith: { $1 })         
     }
     
     open func request<T:EndPointDescriptor>(endPoint:T) -> AnyPublisher<URLRequest,HTTPError> {
@@ -50,8 +63,8 @@ open class BaseHTTPClient : HTTPInterface {
                 let request = try URLRequest(baseURL: self?.baseURL ?? URL(string: "http://")!)
                     .method(endPoint.method)
                     .path(endPoint.path)
-                    .params(endPoint.params)
-                    .headers(self?.headers(endPoint: endPoint) ?? [:])
+                    .params(self?.urlParams(endPoint) ?? [:])
+                    .headers(self?.headers(endPoint) ?? [:])
                     .body(endPoint.requestBodySerialiser?.serialise())
                              
                 promise(.success(request))
