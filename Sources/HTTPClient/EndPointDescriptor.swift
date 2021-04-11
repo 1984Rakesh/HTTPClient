f9d619e971bb069c82703a62ce1ad615c7d00864
Rakesh Patole <patolerakesh@gmail.com>
2021-04-11 14:33:40 +0530

    chores

--- Sources/HTTPClient/EndPointDescriptor.swift
+++ Sources/HTTPClient/EndPointDescriptor.swift
@@ -9,18 +9,22 @@ import Foundation
 
 public protocol EndPointDescriptor {
     associatedtype RequestBodySerialiser : HTTPRequestBodySerialiser
+    associatedtype ParentEndPointDescriptor: EndPointDescriptor
     associatedtype Output
-    var path : String { get }
-    var method : HTTPMethod { get }
-    var params : [String:String] { get }
-    var headers : [String:String] { get }
-    var requestBodySerialiser : RequestBodySerialiser? { get}
+    
+    var parent: ParentEndPointDescriptor? { get }
+    var path: String { get }
+    var method: HTTPMethod { get }
+    var params: [String:String] { get }
+    var headers: [String:String] { get }
+    var requestBodySerialiser: RequestBodySerialiser? { get}
     func deserializeResponse(_ data:Data, response:URLResponse) throws -> Output
 }
 
 public extension EndPointDescriptor {
-    var method : HTTPMethod { .get }
-    var params : [String:String] { [:] }
-    var headers : [String:String] { [:] }
-    var path : String { "" }
+    var path: String { "" }
+    var method: HTTPMethod { .get }
+    var params: [String:String] { [:] }
+    var headers: [String:String] { [:] }
+    var parent: VoidEndPointDescriptor? { nil }
 }
